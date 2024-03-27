4ae650b61fc96e08b630012c490de95d6582343b
C0deFixer <sharipovdmi@gmail.com>
2024-03-27 17:40:39 +0300

    1 step done

---
+++ app/src/main/java/hexlet/code/App.java
@@ -0,0 +1,85 @@
+package hexlet.code;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import hexlet.code.dto.MainPage;
+import io.javalin.rendering.template.JavalinJte;
+
+import static io.javalin.rendering.template.TemplateUtil.model;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import io.javalin.Javalin;
+import io.javalin.validation.ValidationException;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class App {
+    //test branches
+    private static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", "7070");
+        return Integer.valueOf(port);
+    }
+    //locally need can't be run on localhost
+    private static String getAdress() {
+        return System.getenv().getOrDefault("ADRESS", "127.0.0.1");
+    }
+
+    private static String readResourceFile(String fileName) throws IOException {
+        var inputStream = App.class.getClassLoader().getResourceAsStream(fileName);
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+            return reader.lines().collect(Collectors.joining("\n"));
+        }
+    }
+
+    public static void main(String[] args) throws IOException, SQLException {
+        var app = getApp();
+
+        app.start(getAdress(),getPort());
+    }
+
+    public static Javalin getApp() throws IOException, SQLException {
+        // System.setProperty("h2.traceLevel", "TRACE_LEVEL_SYSTEM_OUT=4");
+
+        var hikariConfig = new HikariConfig();
+        hikariConfig.setJdbcUrl("jdbc:h2:mem:project;DB_CLOSE_DELAY=-1;");
+
+        var dataSource = new HikariDataSource(hikariConfig);
+ //      var sql = readResourceFile("schema.sql");
+
+//        log.info(sql);
+//        try (var connection = dataSource.getConnection();
+//             var statement = connection.createStatement()) {
+//            statement.execute(sql);
+//        }
+
+        //BaseRepository.dataSource = dataSource;
+
+        var app = Javalin.create(config -> {
+            config.bundledPlugins.enableDevLogging();
+            config.fileRenderer(new JavalinJte());
+        });
+
+        app.before(ctx -> {
+            ctx.contentType("text/html; charset=utf-8");
+        });
+        app.get("/",ctx -> {
+
+            //ctx.render("index.jte", model("page",page));
+            var visited = Boolean.valueOf(ctx.cookie("visited"));
+            //var page = new MainPage(visited, ctx.sessionAttribute("currentUser"));
+            MainPage page = new MainPage(visited,"Dmitry test");
+            ctx.render("index.jte", model("page", page));
+            ctx.cookie("visited", String.valueOf(true));
+        });
+
+        return app;
+    }
+}
---
+++ app/src/main/java/hexlet/code/util/NamedRoutes.java
@@ -0,0 +1,11 @@
+package hexlet.code.util;
+
+public class NamedRoutes {
+    public static String sessionsPath() {
+        return "/sessions";
+    }
+
+    public static String buildSessionPath() {
+        return "/sessions/build";
+    }
+}

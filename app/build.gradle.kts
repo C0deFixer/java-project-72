ea9f8e0d9edb4b71b8a22339695dff1759adecaa
C0deFixer <sharipovdmi@gmail.com>
2024-03-28 14:56:34 +0300

    jte path setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,6 +13,7 @@ application {
     mainClass.set("hexlet.code.App")
 }
 
+
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
 
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -8,6 +8,9 @@ import java.sql.SQLException;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import gg.jte.ContentType;
+import gg.jte.TemplateEngine;
+import gg.jte.resolve.ResourceCodeResolver;
 import hexlet.code.dto.MainPage;
 import io.javalin.rendering.template.JavalinJte;
 
@@ -27,6 +30,7 @@ public class App {
         String port = System.getenv().getOrDefault("PORT", "7070");
         return Integer.valueOf(port);
     }
+
     //locally need can't be run on localhost
     private static String getAdress() {
         return System.getenv().getOrDefault("ADRESS", "127.0.0.1");
@@ -42,7 +46,7 @@ public class App {
     public static void main(String[] args) throws IOException, SQLException {
         var app = getApp();
 
-        app.start(getAdress(),getPort());
+        app.start(getAdress(), getPort());
     }
 
     public static Javalin getApp() throws IOException, SQLException {
@@ -52,34 +56,41 @@ public class App {
         hikariConfig.setJdbcUrl("jdbc:h2:mem:project;DB_CLOSE_DELAY=-1;");
 
         var dataSource = new HikariDataSource(hikariConfig);
- //      var sql = readResourceFile("schema.sql");
+        //      var sql = readResourceFile("schema.sql");
 
 //        log.info(sql);
 //        try (var connection = dataSource.getConnection();
 //             var statement = connection.createStatement()) {
 //            statement.execute(sql);
 //        }
-
-        //BaseRepository.dataSource = dataSource;
-
         var app = Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
-            config.fileRenderer(new JavalinJte());
+            config.fileRenderer(new JavalinJte(createTemplateEngine()));
         });
+
+        //BaseRepository.dataSource = dataSource;
 
         app.before(ctx -> {
             ctx.contentType("text/html; charset=utf-8");
         });
-        app.get("/",ctx -> {
 
+
+        app.get("/", ctx -> {
             //ctx.render("index.jte", model("page",page));
             var visited = Boolean.valueOf(ctx.cookie("visited"));
             //var page = new MainPage(visited, ctx.sessionAttribute("currentUser"));
-            MainPage page = new MainPage(visited,"Dmitry test");
+            MainPage page = new MainPage(visited, "Dmitry test");
             ctx.render("index.jte", model("page", page));
             ctx.cookie("visited", String.valueOf(true));
         });
 
         return app;
+    }
+
+    private static TemplateEngine createTemplateEngine() {
+        ClassLoader classLoader = App.class.getClassLoader();
+        ResourceCodeResolver codeResolver = new ResourceCodeResolver("templates", classLoader);
+        TemplateEngine templateEngine = TemplateEngine.create(codeResolver, ContentType.Html);
+        return templateEngine;
     }
 }

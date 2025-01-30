97199a3f0b7b4a2cb02e296034f23c2a3d14190d
Dmitry Sharypov <Sharypov_D@mail.ru>
2025-01-30 23:41:01 +0300

    Switch off unused inmport classes

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -22,7 +22,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.junitpioneer.jupiter.SetEnvironmentVariable;
 
 import java.io.BufferedReader;
 import java.io.IOException;
